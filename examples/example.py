ab2e3ab11f5a3625d9ef729507871c6061c9aefa
Romain Beaumont <romain.rom1@gmail.com>
2022-05-10 00:14:57 +0200

    first example

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,15 @@
 from distributed_translator import hello_world
 
 hello_world("hi")
+
+from easynmt import EasyNMT
+model = EasyNMT('opus-mt')
+
+#Translate a single sentence to German
+print(model.translate('This is a sentence we want to translate to German', target_lang='de'))
+
+#Translate several sentences to German
+sentences = ['You can define a list with sentences.',
+             'All sentences are translated to your target language.',
+             'Note, you could also mix the languages of the sentences.']
+print(model.translate(sentences, target_lang='de'))
